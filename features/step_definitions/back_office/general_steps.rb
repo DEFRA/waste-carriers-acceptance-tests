be0f9eca451b0967eddd3878f8765590c996d233
Tim Stone <tim.stone@yahoo.co.uk>
2017-10-02 13:00:45 +0100

    adding feature file

---
+++ features/step_definitions/back_office/general_steps.rb
@@ -0,0 +1,7 @@
+When(/^I complete my registration of my limited company as a upper tier waste carrier$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I will be registered as an upper tier waste carrier$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
