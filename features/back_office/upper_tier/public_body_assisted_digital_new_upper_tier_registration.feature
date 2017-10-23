4a7737918c1fa8914aec6ed19d2f9929c6d086ec
Tim Stone <tim.stone@yahoo.co.uk>
2017-10-23 14:15:06 +0100

    removing wip tag

--- features/back_office/upper_tier/public_body_assisted_digital_new_upper_tier_registration.feature
+++ features/back_office/upper_tier/public_body_assisted_digital_new_upper_tier_registration.feature
@@ -1,4 +1,4 @@
-@backoffice @upper_tier @wip
+@backoffice @upper_tier
 Feature: Assisted digital registration of an upper tier public body
   As a carrier of commerical waste
   I want assistance with my waste carrier registration from the Environment Agency
--- features/step_definitions/back_office/upper_tier/public_body_assisted_digital_new_upper_tier_registration_steps.rb
+++ features/step_definitions/back_office/upper_tier/public_body_assisted_digital_new_upper_tier_registration_steps.rb
@@ -1,23 +1,23 @@
 When(/^I have my public body upper tier waste carrier application completed for me$/) do
-    @app.business_type_page.submit(org_type: "publicBody")
-    @app.other_businesses_question_page.submit(choice: :no)
-    @app.construction_waste_question_page.submit(choice: :yes)
-    @app.registration_type_page.submit(choice: :broker_dealer)
-    @app.business_details_page.submit(
+  @app.business_type_page.submit(org_type: "publicBody")
+  @app.other_businesses_question_page.submit(choice: :no)
+  @app.construction_waste_question_page.submit(choice: :yes)
+  @app.registration_type_page.submit(choice: :broker_dealer)
+  @app.business_details_page.submit(
     company_name: "AD UT Public Body",
     postcode: "BS1 5AH",
     result: "ENVIRONMENT AGENCY, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH"
-    )
-    @app.contact_details_page.submit(
+  )
+  @app.contact_details_page.submit(
     first_name: "Bob",
     last_name: "Debuilder",
     phone_number: "012345678"
-    )
-    @app.postal_address_page.submit
+  )
+  @app.postal_address_page.submit
 
-    people = @app.key_people_page.key_people
-    @app.key_people_page.submit_key_person(person: people[0])
+  people = @app.key_people_page.key_people
+  @app.key_people_page.submit_key_person(person: people[0])
 
-    @app.relevant_convictions_page.submit(choice: :no)
-    @app.declaration_page.submit
+  @app.relevant_convictions_page.submit(choice: :no)
+  @app.declaration_page.submit
 end
