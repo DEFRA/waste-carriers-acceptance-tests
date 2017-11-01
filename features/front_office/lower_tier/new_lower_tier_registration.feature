936024df56728133c4b5cf181def7fcd7ebb3e06
Tim Stone <tim.stone@yahoo.co.uk>
2017-11-01 12:16:36 +0000

    updating lower tier tests to include email confirmation steps

--- features/front_office/lower_tier/new_lower_tier_registration.feature
+++ features/front_office/lower_tier/new_lower_tier_registration.feature
@@ -5,38 +5,39 @@ Feature: New lower tier registrations
   So I am compliant with the law
    
  Scenario: Charity successfully registers for a lower tier waste carriers licence
-   Given I start a new registration
-   	When I complete my registration of my charity as a lower tier waste carrier
-   	Then I will be asked to confirm my email address
+   Given I complete my application of my charity as a lower tier waste carrier
+    When I confirm my email address
+    Then I will be registered as a lower tier waste carrier
+     And I will have received a registration complete confirmation email 
 
  Scenario: Sole trader successfully registers for a lower tier waste carriers licence
-   Given I start a new registration
-   	When I complete my registration of a sole trader business as a lower tier waste carrier
-   	Then I will be asked to confirm my email address
+   Given I complete my application of a sole trader business as a lower tier waste carrier
+    When I confirm my email address
+    Then I will be registered as a lower tier waste carrier
+     And I will have received a registration complete confirmation email 
 
  Scenario: Public body successfully registers for a lower tier waste carriers licence
-   Given I start a new registration
-   	When I complete my registration of my public body as a lower tier waste carrier
-   	Then I will be asked to confirm my email address
+   Given I complete my application of my public body as a lower tier waste carrier
+    When I confirm my email address
+    Then I will be registered as a lower tier waste carrier
+     And I will have received a registration complete confirmation email 
 
  Scenario: Partnership successfully registers for a lower tier waste carriers licence
-   Given I start a new registration
-   	When I complete my registration of my partnership as a lower tier waste carrier
-   	Then I will be asked to confirm my email address
+   Given I complete my application of my partnership as a lower tier waste carrier
+   	When I confirm my email address
+    Then I will be registered as a lower tier waste carrier
+     And I will have received a registration complete confirmation email 
 
  Scenario: Local authority successfully registers for a lower tier waste carriers licence
-   Given I start a new registration
-   	When I complete my registration of my local authority as a lower tier waste carrier
-   	Then I will be asked to confirm my email address
+   Given I complete my application of my local authority as a lower tier waste carrier
+   	When I confirm my email address
+    Then I will be registered as a lower tier waste carrier
+     And I will have received a registration complete confirmation email 
 @wip
  Scenario: Limited company successfully registers for a lower tier waste carriers licence
-   Given I have completed my application of my limited company as a lower tier waste carrier
+   Given I complete my application of my limited company as a lower tier waste carrier
     When I confirm my email address
    	Then I will be registered as a lower tier waste carrier
      And I will have received a registration complete confirmation email 
 
- Scenario: Lower tier limited company applies for a lower tier waste carriers licence
-   Given I start a new registration
-    When I complete my application of my limited company as a lower tier waste carrier
-    Then I will be asked to confirm my email address
      
--- features/step_definitions/front_office/new_lower_tier_registration_steps.rb
+++ features/step_definitions/front_office/new_lower_tier_registration_steps.rb
@@ -1,4 +1,7 @@
-When(/^I complete my registration of my charity as a lower tier waste carrier$/) do
+When(/^I complete my application of my charity as a lower tier waste carrier$/) do
+  @app = FrontOfficeApp.new
+  @app.start_page.load
+  @app.start_page.submit
   @app.business_type_page.submit(org_type: "charity")
   @app.business_details_page.submit(
     company_name: "LT charity",
@@ -20,36 +23,12 @@ When(/^I complete my registration of my charity as a lower tier waste carrier$/)
     confirm_password: "Secret123",
     confirm_email: @email
   )
-  puts @email
 end
 
-Given(/^I complete my application of my limited company as a lower tier waste carrier$/) do
-  @app.business_type_page.submit(org_type: "limitedCompany")
-  @app.other_businesses_question_page.submit(choice: :no)
-  @app.construction_waste_question_page.submit(choice: :no)
-  @app.business_details_page.submit(
-    company_name: "LT Company limited",
-    postcode: "BS1 5AH",
-    result: "ENVIRONMENT AGENCY, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH"
-  )
-  @email = @app.generate_email
-  @app.contact_details_page.submit(
-    first_name: "Bob",
-    last_name: "Carolgees",
-    phone_number: "012345678",
-    email: @email
-  )
-  @app.postal_address_page.submit
-  @app.declaration_page.submit
-
-  @app.sign_up_page.submit(
-    registration_password: "Secret123",
-    confirm_password: "Secret123",
-    confirm_email: @email
-  )
-end
-
-When(/^I complete my registration of my local authority as a lower tier waste carrier$/) do
+When(/^I complete my application of my local authority as a lower tier waste carrier$/) do
+  @app = FrontOfficeApp.new
+  @app.start_page.load
+  @app.start_page.submit
   @app.business_type_page.submit(org_type: "authority")
   @app.business_details_page.submit(
     company_name: "LT local athority",
@@ -73,7 +52,10 @@ When(/^I complete my registration of my local authority as a lower tier waste ca
   )
 end
 
-When(/^I complete my registration of my partnership as a lower tier waste carrier$/) do
+When(/^I complete my application of my partnership as a lower tier waste carrier$/) do
+  @app = FrontOfficeApp.new
+  @app.start_page.load
+  @app.start_page.submit
   @app.business_type_page.submit(org_type: "partnership")
   @app.other_businesses_question_page.submit(choice: :yes)
   @app.service_provided_question_page.submit(choice: :not_main_service)
@@ -100,7 +82,10 @@ When(/^I complete my registration of my partnership as a lower tier waste carrie
   )
 end
 
-When(/^I complete my registration of my public body as a lower tier waste carrier$/) do
+When(/^I complete my application of my public body as a lower tier waste carrier$/) do
+  @app = FrontOfficeApp.new
+  @app.start_page.load
+  @app.start_page.submit
   @app.business_type_page.submit(org_type: "publicBody")
   @app.other_businesses_question_page.submit(choice: :no)
   @app.construction_waste_question_page.submit(choice: :no)
@@ -126,7 +111,10 @@ When(/^I complete my registration of my public body as a lower tier waste carrie
   )
 end
 
-Given(/^I complete my registration of a sole trader business as a lower tier waste carrier$/) do
+Given(/^I complete my application of a sole trader business as a lower tier waste carrier$/) do
+  @app = FrontOfficeApp.new
+  @app.start_page.load
+  @app.start_page.submit
   @app.business_type_page.submit(org_type: "soleTrader")
   @app.other_businesses_question_page.submit(choice: :yes)
   @app.service_provided_question_page.submit(choice: :not_main_service)
@@ -153,7 +141,7 @@ Given(/^I complete my registration of a sole trader business as a lower tier was
   )
 end
 
-Given(/^I have completed my application of my limited company as a lower tier waste carrier$/) do
+Given(/^I complete my application of my limited company as a lower tier waste carrier$/) do
   @app = FrontOfficeApp.new
   @app.start_page.load
   @app.start_page.submit
