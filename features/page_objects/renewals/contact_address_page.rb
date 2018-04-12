b85d848852b6c995a9049b7ec926e535bbdbf458
timstone <tim.stone@yahoo.co.uk>
2018-04-12 15:34:27 +0100

    adding contact address lookup from postcode

--- features/page_objects/renewals/contact_address_page.rb
+++ features/page_objects/renewals/contact_address_page.rb
@@ -1,12 +1,15 @@
 class ContactAddressPage < SitePrism::Page
 
   # whats the address of the person we should contact?
+  element(:change_postcode, "a[href*='skip_to_manual_address']")
+  element(:address, "#contact_address_form_temp_address")
+  element(:results_dropdown, "select#contact_address_form_temp_address")
+  element(:manual_address, "a[href*='contact-address']")
+  element(:submit_button, "input[value='Continue']")
 
-  element(:submit_button, "input[type='Submit']")
-  element(:heading, :xpath, "//h1[contains(text(), 'address')]")
-
-  def submit(_args = {})
-    wait_for_heading
+  def submit(args = {})
+    wait_for_results_dropdown
+    results_dropdown.select(args[:result]) if args.key?(:result)
     submit_button.click
   end
 
