0abeedff5ab4f3d93f9ee413ec2a6a4844cc077d
timstone <tim.stone@yahoo.co.uk>
2018-11-13 11:54:45 +0000

    transfer registration to another account

--- features/step_definitions/back_office/change_account_email_steps.rb
+++ features/step_definitions/back_office/change_account_email_steps.rb
@@ -1,98 +1,18 @@
-require "faker"
-
-Given(/I have a registration "([^"]*)"$/) do |reg_no|
-  # reserves registration number for specific test
-  @registration_number = reg_no
-end
-
-Given(/^the user has 2 registrations$/) do
-  @front_app = FrontOfficeApp.new
-  @first_name = Faker::Name.first_name
-  @last_name = Faker::Name.last_name
-  @email_address = @front_app.generate_email
-  @registrations = []
-
-  @front_app.start_page.load
-  @front_app.start_page.submit
-  @front_app.business_type_page.submit(org_type: "charity")
-
-  @front_app.business_details_page.submit(
-    company_name: Faker::Company.name,
-    postcode: "BS1 5AH",
-    result: "NATURAL ENGLAND, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH"
-  )
-
-  @front_app.contact_details_page.submit(
-    first_name: @first_name,
-    last_name: @last_name,
-    phone_number: "012345678",
-    email: @email_address
-  )
-
-  @front_app.postal_address_page.submit
-  @front_app.check_details_page.submit
-
-  @front_app.sign_up_page.submit(
-    registration_password: ENV["WCRS_DEFAULT_PASSWORD"],
-    confirm_password: ENV["WCRS_DEFAULT_PASSWORD"],
-    confirm_email: @email_address
-  )
-  @front_app.start_page.load
-  @front_app.start_page.submit
-  @front_app.business_type_page.submit(org_type: "charity")
-
-  @front_app.business_details_page.submit(
-    company_name: Faker::Company.name,
-    postcode: "BS1 5AH",
-    result: "NATURAL ENGLAND, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH"
-  )
-
-  @front_app.contact_details_page.submit(
-    first_name: @first_name,
-    last_name: @last_name,
-    phone_number: "012345678",
-    email: @email_address
-  )
-
-  @front_app.postal_address_page.submit
-  @front_app.check_details_page.submit
-
-  @front_app.waste_carrier_sign_in_page.submit(
-    password: ENV["WCRS_DEFAULT_PASSWORD"]
-  )
-
-  @registrations << @front_app.confirmation_page.registration_number.text
-end
-
-When(/^I change the account email$/) do
+Given(/^I choose to transfer ownership of "([^"]*)" to another user$/) do |reg|
+  @registration_number = reg
   @back_app.registrations_page.search(search_input: @registration_number)
-  @back_app.registrations_page.search_results[0].change_account_email.click
-
-  @new_email = @back_app.generate_email
-  @back_app.edit_account_email_page.submit(email: @new_email)
+  @back_app.registrations_page.search_results[0].transfer.click
 end
 
-When(/^I change the account email for both$/) do
-  @registrations.each do |_reg_no|
-    @back_app.registrations_page.search(search_input: @registration_number)
-    @back_app.registrations_page.search_results[0].change_account_email.click
-
-    @new_email = @front_app.generate_email
-    @back_app.edit_account_email_page.submit(email: @new_email)
-
-    expect(@back_app.edit_account_email_page).to have_notice
-
-    @back_app.edit_account_email_page.back_link.click
-  end
+When(/^I change the account email to "([^"]*)"$/) do |email|
+  @back_renewals_app.transfer_registration_page.submit(
+    email: email,
+    confirm_email: email
+  )
+  # saves email value for later on
+  @email = email
 end
 
 Then(/^I see a confirmation the change has been made$/) do
-  expect(@back_app.edit_account_email_page).to have_notice
-end
-
-Then(/^I see a confirmation for both$/) do
-  # I do nothing. The expect() call actually takes place in
-  # `I change the account email for both` because there is no way to see the
-  # notice other than at the time of making the change.
-  # Having this step just makes the scenario read better :-)
+  expect(@back_renewals_app.transfer_registration_page).to have_text(@email)
 end
