34f233d75396109ed9a79cbf20b1b18b36a5f297
Andrew Hick <andrewhick.ea@gmail.com>
2020-07-09 16:23:26 +0100

    Add untracked files

---
+++ features/page_objects/journey/start_page.rb
@@ -0,0 +1,20 @@
+class StartPage < SitePrism::Page
+  set_url("#{Quke::Quke.config.custom['urls']['front_office']}/start")
+
+  element(:error_summary, ".error-summary")
+
+  element(:new_registration, "input[value='new']", visible: false)
+  element(:renew_registration, "input[value='renew']", visible: false)
+
+  element(:submit_button, "input[type='submit']")
+
+  def submit(args = {})
+    if args[:choice] == :renewal
+      renew_registration.click
+    elsif args[:choice] == :new_registration
+      new_registration.click
+    end
+
+    submit_button.click
+  end
+end
