50f7d8905b9f655a67d7345e6f74b0173fd17ea9
Andrew Hick <andrewhick.ea@gmail.com>
2020-10-12 08:36:36 +0100

    Register outside england (#368)

--- features/page_objects/journey/confirm_business_type_page.rb
+++ features/page_objects/journey/confirm_business_type_page.rb
@@ -3,6 +3,7 @@ class ConfirmBusinessTypePage < SitePrism::Page
   # What type of business or organisation are you? - front office
 
   element(:error_summary, ".error-summary")
+  element(:heading, ".heading-large")
 
   elements(:org_types, "input[name='business_type_form[business_type]']", visible: false)
   element(:submit_button, "input[type='submit']", visible: false)
