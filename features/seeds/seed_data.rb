1ea534134f65cee4f2f94aaf86a89c3011c9f346
timstone <tim.stone@yahoo.co.uk>
2021-04-12 16:20:09 +0100

    pm4

--- features/seeds/seed_data.rb
+++ features/seeds/seed_data.rb
@@ -41,9 +41,14 @@ class SeedData
     request = Net::HTTP::Post.new(uri, "Content-Type" => "application/json")
     request.body = data
 
-    proxy_uri = URI.parse("http://apsldnprxelb001.aws-int.defra.cloud:3128")
-
-    http = Net::HTTP.new(uri.hostname, uri.port, proxy_uri.host, proxy_uri.port)
+    if ENV["WCRS_PROXY"].nil?
+      http = Net::HTTP.new(uri.hostname, uri.port)
+      puts "no proxy"
+    else
+      puts "using proxy"
+      proxy_uri = URI.parse(ENV["WCRS_PROXY"])
+      http = Net::HTTP.new(uri.hostname, uri.port, proxy_uri.host, proxy_uri.port)
+    end
     http.use_ssl = true
     http.set_debug_output($stdout)
 
