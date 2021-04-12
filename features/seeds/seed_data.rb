78b0f60e0c854d2ad1e032b21ec8184bd255ecae
timstone <tim.stone@yahoo.co.uk>
2021-04-12 10:49:19 +0100

    Fix 4

--- features/seeds/seed_data.rb
+++ features/seeds/seed_data.rb
@@ -41,17 +41,9 @@ class SeedData
     request = Net::HTTP::Post.new(uri, "Content-Type" => "application/json")
     
     request.body = data
-    proxy_uri = URI.parse('http://apsldnprxelb001.aws-int.defra.cloud:3128')
-    # Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
-    #   puts "Open timeout is #{http.open_timeout}"
-    #   puts "Read timeout is #{http.read_timeout}"
-    #   http.set_debug_output($stdout)
-    #   http.request(request)
-    # end
+    proxy_uri = URI.parse("#{Quke::Quke.config.custom["proxy"]["host"]}:#{Quke::Quke.config.custom["proxy"]["port"]}")
     http = Net::HTTP.new(uri.hostname, uri.port, proxy_uri.host, proxy_uri.port)
     http.use_ssl = true
-    http.set_debug_output($stdout)
-    
 
     http.request(request)
 
