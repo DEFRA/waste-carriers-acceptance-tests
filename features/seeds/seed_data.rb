23ea208557591c92a7be22a5cb9f3b915e41f2fc
timstone <tim.stone@yahoo.co.uk>
2021-04-16 09:24:55 +0100

    Renewal window grace period tests update

--- features/seeds/seed_data.rb
+++ features/seeds/seed_data.rb
@@ -46,7 +46,7 @@ class SeedData
       proxy_uri = URI.parse(ENV["WCRS_PROXY"])
       http = Net::HTTP.new(uri.hostname, uri.port, proxy_uri.host, proxy_uri.port)
     end
-    http.use_ssl = true
+    http.use_ssl = true unless uri.to_s.include?("http://")
     http.request(request)
   end
 
