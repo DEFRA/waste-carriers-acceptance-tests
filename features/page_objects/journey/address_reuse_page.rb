3f6fbea0b52d47936fa4a133cc65ff1c3d043ec8
timstone <tim.stone@yahoo.co.uk>
2021-12-02 10:51:36 +0000

    Page object clean up

--- features/page_objects/journey/address_reuse_page.rb
+++ features/page_objects/journey/address_reuse_page.rb
@@ -2,9 +2,6 @@ class AddressReusePage < BasePage
 
   # Do you want to use this address as a contact address?
 
-  element(:error_summary, ".govuk-error-summary__body")
-  element(:heading, "h1")
-
   element(:reuse, "[value='yes']+ .govuk-radios__label")
   element(:no_reuse, "[value='no']+ .govuk-radios__label")
   element(:submit_button, "[type='submit']")
