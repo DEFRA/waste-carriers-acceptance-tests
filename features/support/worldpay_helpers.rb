10e68d15c2979bafc5907f3f82e1ee7c845d96a8
Tim <tim.stone@yahoo.co.uk>
2022-02-04 16:17:19 +0000

    Payment integration tests update

--- features/support/worldpay_helpers.rb
+++ features/support/worldpay_helpers.rb
@@ -2,11 +2,7 @@
 
 def submit_valid_card_payment
   sleep(3)
-  # The return clause is called if WorldPay mocking is in place - if so, the payment is skipped and marked as complete.
-  worldpay_page_is_shown = @journey.worldpay_payment_page.has_test_mode_text?
-  return unless worldpay_page_is_shown
-
-  expect(@journey.worldpay_payment_page.test_mode_text).to have_text("Test Mode - This is not a live transaction")
+  return if mocking_enabled?
 
   @journey.worldpay_payment_page.submit(
     card_number: "6759649826438453",
@@ -22,7 +18,6 @@ end
 
 def submit_invalid_card_payment
   sleep(3)
-  expect(@journey.worldpay_payment_page.test_mode_text).to have_text("Test Mode - This is not a live transaction")
   @journey.worldpay_payment_page.submit(
     card_number: "6759649826438453",
     cardholder_name: "3d.refused",
