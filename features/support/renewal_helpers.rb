e050b3c1bd63305bf4156ed9e527bb28e229b01e
Tim <tim.stone@yahoo.co.uk>
2022-03-17 17:29:43 +0000

    wip

--- features/support/renewal_helpers.rb
+++ features/support/renewal_helpers.rb
@@ -27,7 +27,7 @@ def submit_existing_renewal_details
   submit_carrier_details("existing", "existing", "existing")
   expect(@journey.renewal_information_page).to have_text("you still need an upper tier registration")
   @journey.renewal_information_page.submit
-  submit_business_details(@business_name, @tier)
+  submit_business_renewal_details(@business_name, @tier)
   submit_company_people
   submit_convictions(@convictions)
   submit_contact_details_for_renewal
@@ -42,4 +42,31 @@ def expiry_date_from_reg_details
 
   expiry_date = page.text.match(/.*Expires: (.*)\n.*/)[1]
   Date.parse(expiry_date)
+end
+
+def submit_business_renewal_details(business_name, tier)
+  # submits company number, name and address
+  if @journey.company_number_page.heading.has_text? "What's the registration number"
+    # then it's a limited company or LLP:
+    submit_limited_company_renewal_details(business_name, tier)
+  else
+    # it'll be the company name page, which will have a heading like "What's the name of the business?"
+    submit_organisation_details(business_name)
+  end
+end
+
+def submit_limited_company_renewal_details(business_name, tier)
+  # Submit company number:
+  if tier == "upper"
+    @companies_house_number ||= "00445790"
+    @journey.company_number_page.submit(companies_house_number: @companies_house_number)
+  end
+
+  if business_name == "existing"
+    @journey.company_name_page.submit
+  else
+    @journey.company_name_page.submit(company_name: business_name)
+  end
+
+  complete_address_with_random_method
 end
