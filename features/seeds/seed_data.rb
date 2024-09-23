2a6501fab022b4d0c93e76300891fda30277b3c0
Tim <tim.stone@yahoo.co.uk>
2024-09-23 15:02:52 +0100

    seed test

--- features/seeds/seed_data.rb
+++ features/seeds/seed_data.rb
@@ -46,7 +46,7 @@ class SeedData
       proxy_uri = URI.parse(ENV["WCRS_PROXY"])
       http = Net::HTTP.new(uri.hostname, uri.port, proxy_uri.host, proxy_uri.port)
     end
-    http.use_ssl = true unless uri.to_s.include?("http://")
+    http.use_ssl = true
     http.request(request)
   end
 
